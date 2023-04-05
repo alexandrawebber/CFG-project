43a3c25b63931d4467b5e289d8a6dac18633ad65
alexandrawebber <125410015+alexandrawebber@users.noreply.github.com>
2023-04-05 18:36:31 +0100

    Add files via upload

---
+++ website.js
@@ -0,0 +1,44 @@
+document.querySelector("#show-login").addEventListener("click",function(){
+    document.querySelector(".popup").classList.add("active");
+});
+
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+    document.querySelector(".popup").classList.remove("active");
+});
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+      const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+      const slides = button
+       .closest("[data-carousel]")
+       .querySelector("[data-slides]");
+
+  const activeSlide = slides.querySelector("[data-active]")
+  let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+  if (newIndex < 0) newIndex = slides.children.length - 1;
+  if (newIndex >= slides.children.length) newIndex = 0;
+
+  slides.children[newIndex].dataset.active = true;
+  delete activeSlide.dataset.active;
+  });
+});
+
+const filters = document.querySelectorAll('.carousel__filter');
+const slides = document.querySelectorAll('.carousel__slide');
+
+filters.forEach(filter => {
+  filter.addEventListener('click', (e) => {
+    const category = e.target.getAttribute('data-filter');
+    slides.forEach(slide => {
+      if (category === 'all') {
+        slide.style.display = 'block';
+      } else if (slide.getAttribute('data-filter') === category) {
+        slide.style.display = 'block';
+      } else {
+        slide.style.display = 'none';
+      }
+    });
+    });
+  });
